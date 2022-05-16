bf8e0d6f9f4586f7588177ed6430d94140c273a6
Tim Rolshud <tim.rolsh@gmail.com>
2022-05-16 00:54:50 -0400

    did some steps not sure if it works tho

--- huff/HuffProcessor.java
+++ huff/HuffProcessor.java
@@ -38,7 +38,7 @@ public class HuffProcessor implements Processor {
         in.reset();
 
         // Write the body of the compressed file, followed by EOF.
-        writeCompressedBits (in, codings, out);
+        writeCompressedBits(in, codings, out);
 
         // Make sure all bits have been physically written to the file.
         out.flush();
@@ -49,34 +49,39 @@ public class HuffProcessor implements Processor {
 
     }
 
-    /** Returns an array [0-255] that represents the frequency of each byte value in
-     * the input file. */
-    private int[] readForCounts(BitInputStream in)
-    {
+    /**
+     * Returns an array [0-255] that represents the frequency of each byte value in
+     * the input file.
+     */
+    private int[] readForCounts(BitInputStream in) {
+        // array index is the byte, array value at index is count of byte
         int[] arr = new int[256];
         int ind = in.readBits(8);
-        while(ind > 0) {
-            arr[ind] += 1;
+        // while we havent hit the EOF indicator (-1), read bytes in and fill array with
+        // their counts =
+        while (ind != -1) {
+            ++arr[ind];
             ind = in.readBits(8);
         }
         return arr;
     }
 
-
-    /** Debugging method.
+    /**
+     * Debugging method.
      * Prints the counts in the array.
-     * */
-    public void printCounts (int[] counts) {
-        for (int i=0; i<counts.length; i++) {
+     */
+    public void printCounts(int[] counts) {
+        for (int i = 0; i < counts.length; i++) {
             if (counts[i] > 0) {
                 System.out.printf("%d (%c): %d\n", i, i > 32 ? i : ' ', counts[i]);
             }
         }
     }
 
-    /** Uses the array from readForCounts and returns a HuffNode that's
+    /**
+     * Uses the array from readForCounts and returns a HuffNode that's
      * the root of the Huffman coding tree. Use a priority queue to create
-     * the tree, as we did in class.  Be sure to create a count of one for
+     * the tree, as we did in class. Be sure to create a count of one for
      * PSEUDO_EOF and include it in the priority queue (and tree) as well.
      */
     private HuffNode makeTreeFromCounts(int[] array) {
@@ -88,20 +93,30 @@ public class HuffProcessor implements Processor {
                 priorityQueue.add(new HuffNode(a, array[a]));
             }
         }
-        HuffNode pointer = priorityQueue.remove()
-        // TODO finish step 2 i didnt finish this yet
+
+        // add EOF character to queue, it only occurs once and its value is -1
+        priorityQueue.add(new HuffNode(-1, 1));
+
+        while (priorityQueue.size() > 1) {
+            HuffNode one = priorityQueue.remove();
+            HuffNode two = priorityQueue.remove();
+            priorityQueue.add(new HuffNode(0, one.weight() + two.weight(), one, two));
+        }
+        return priorityQueue.remove();
     }
 
-    /** Debugging method.
-     * This is very similar to the prior lab.  I am providing the implementation so that our trees
-     * have a consistent appearance. */
+    /**
+     * Debugging method.
+     * This is very similar to the prior lab. I am providing the implementation so
+     * that our trees
+     * have a consistent appearance.
+     */
     private void printEncodingTree(HuffNode n, int indent) {
         if (n.isLeaf()) {
             System.out.printf("%" + indent + "s", "");
-            String val = n.value() == PSEUDO_EOF ? "EOF" : "" + (char)n.value();
+            String val = n.value() == PSEUDO_EOF ? "EOF" : "" + (char) n.value();
             System.out.println("\'" + val + "\'");
-        }
-        else {
+        } else {
             System.out.printf("%" + indent + "sL\n", "");
             printEncodingTree(n.left(), indent + 2);
 
@@ -110,25 +125,46 @@ public class HuffProcessor implements Processor {
         }
     }
 
-    /** Returns an array [0-256] -- note extra space for the EOF character
-     * which has a value of 256.  Each position in the array includes a String
-     * of 0's and 1's indicating the compressed encoding for the byte.  In
+    /**
+     * Returns an array [0-256] -- note extra space for the EOF character
+     * which has a value of 256. Each position in the array includes a String
+     * of 0's and 1's indicating the compressed encoding for the byte. In
      * order to determine these encodings, a preorder traversal of the tree is
-     * used.  Each left traversal adds a "0" to the encoding.  Each right
-     * traversal adds a "1" to the encoding.  When a leaf is reached, the
-     * current encoding is added to the array.*/
+     * used. Each left traversal adds a "0" to the encoding. Each right
+     * traversal adds a "1" to the encoding. When a leaf is reached, the
+     * current encoding is added to the array.
+     */
 
     private String[] makeCodingsFromTree(HuffNode root) {
-        // TODO: Step 3! You will need to create a helper recursive method
-        return new String[257];
+        String[] array = new String[257];
+        preOrderTraverse(root, "", array);
+        return array;
     }
 
+    private void preOrderTraverse(HuffNode node, String path, String[] array) {
+        // prevent nullpointerexception
+        if (node == null) {
+            return;
+        }
+        // base case, you hit a leaf with the byte data
+        if (node.left() == null && node.right() == null) {
+            array[node.value()] = path;
+            return;
+        }
+        path += '0';
+        preOrderTraverse(node.left(), path, array);
+        path = path.substring(0, path.length() - 1);
+        path += '1';
+        preOrderTraverse(node.right(), path, array);
+        path = path.substring(0, path.length() - 1);
+    }
 
-    /** Debugging method.
+    /**
+     * Debugging method.
      * Prints the codings of the characters in the array [0-256].
-     * */
-    public void printCodingsArray (String[] codings) {
-        for (int i=0; i<256; i++) {
+     */
+    public void printCodingsArray(String[] codings) {
+        for (int i = 0; i < 256; i++) {
             if (codings[i] != null) {
                 System.out.printf("%d (%c): %s\n", i, i > 32 ? i : ' ', codings[i]);
             }
@@ -137,32 +173,47 @@ public class HuffProcessor implements Processor {
 
     }
 
-    /** Writes the bits for the header to the output stream.
+    /**
+     * Writes the bits for the header to the output stream.
      * Traverse the tree using preorder traversal.
      * For each internal node, write "0" to the output and traverse left and right.
-     * For each leaf, write "1" to the output followed by the 9 bit representation of
+     * For each leaf, write "1" to the output followed by the 9 bit representation
+     * of
      * the value stored in the leaf.
      */
-    private void writeHeader (HuffNode n, BitOutputStream out)
-    {
-        // TODO: Step 4
+    private void writeHeader(HuffNode n, BitOutputStream out) {
+        if (n.left() == null && n.right() == null) {
+            // if you get to leaf, write a 1, then write the value of the that leaf
+            out.writeBits(1, 0b1);
+            out.writeBits(8, n.value());
+            return;
+        } else {
+            out.writeBits(1, 0);
+            writeHeader(n.left(), out);
+            writeHeader(n.right(), out);
+        }
     }
 
-    /** Write the body of the compressed file.  Read through the input stream 8 bits
-     * at time.  Look up the encoding for that byte from "codings".  Write that
-     * encoding to the output stream using the following, which interprets the String
+    /**
+     * Write the body of the compressed file. Read through the input stream 8 bits
+     * at time. Look up the encoding for that byte from "codings". Write that
+     * encoding to the output stream using the following, which interprets the
+     * String
      * as a binary number to convert it to an int and then writes the correct number
      * of bits to represent that number to output:
-     *         int code = Integer.parseInt(strCode, 2);
-     *         out.writeBits(strCode.length(), code);
+     * int code = Integer.parseInt(strCode, 2);
+     * out.writeBits(strCode.length(), code);
      * After reading through all of "in", write PSEUDO_EOF as a final encoded
      * character to the file.
      */
-    private void writeCompressedBits(BitInputStream in, String[] codings, BitOutputStream out)
-    {
-        // TODO: Step 5
-    }
+    private void writeCompressedBits(BitInputStream in, String[] codings, BitOutputStream out) {
 
+        int bits;
+        while (/* bitinputstream can read more bits*/) {
+            bits = in.readBits(8);
+            out.writeBits(codings[bits].length(), Integer.parseInt(codings[bits], 2));
+        }
+    }
 
     @Override
     public void decompress(BitInputStream in, BitOutputStream out) {
@@ -177,31 +228,33 @@ public class HuffProcessor implements Processor {
         out.flush();
     }
 
-    /** Reads the header of the compressed file and returns encoding trie.
-     * This method uses a recursion.  The algorithm is:
+    /**
+     * Reads the header of the compressed file and returns encoding trie.
+     * This method uses a recursion. The algorithm is:
      * Read one bit from the input.
-     * If the bit is a 0, recurse left and right, creating two new HuffNodes.  Then,
-     * return a new HuffNode with the left and right nodes as the new node's children.
-     * If the bit is a 1, read 9 bits from the input and create a HuffNode using these bits.
-     * The weight of the HuffNodes does not matter during decompression and can be set to -1.
+     * If the bit is a 0, recurse left and right, creating two new HuffNodes. Then,
+     * return a new HuffNode with the left and right nodes as the new node's
+     * children.
+     * If the bit is a 1, read 9 bits from the input and create a HuffNode using
+     * these bits.
+     * The weight of the HuffNodes does not matter during decompression and can be
+     * set to -1.
      */
-    HuffNode readHeader (BitInputStream in)
-    {
+    HuffNode readHeader(BitInputStream in) {
         // TODO: Step 6
-        return new HuffNode (-1, -1);
+        return new HuffNode(-1, -1);
     }
 
-    /** Reads the body of the compressed file.  Start at the root of the tree.
+    /**
+     * Reads the body of the compressed file. Start at the root of the tree.
      * Read 1 bit of input.
-     * If the bit is a 0, traverse left.  If 1, traverse right.
-     * If we are now pointing to a leaf, write the value of the leaf to output.  Go
+     * If the bit is a 0, traverse left. If 1, traverse right.
+     * If we are now pointing to a leaf, write the value of the leaf to output. Go
      * back to the top of the tree and traverse again.
      * (If the value of the leaf is PSEUDO_EOF, exit the function.)
      */
-    private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out)
-    {
+    private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
         // TODO: Step 7
     }
-
 
 }
